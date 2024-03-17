7cc9aea1fe858075d0191aa69f02395e8195f16c
JeelPatel225 <163737261+JeelPatel225@users.noreply.github.com>
2024-03-17 18:12:25 +0530

    Add files via upload

---
+++ c2(ins_des).php
@@ -0,0 +1,16 @@
+<!-- Insert design form -->
+<html>
+<body>
+
+	<form action="c3(insert).php" method="post">
+		Enter Emp No:     <input type="text" name="empno"> <br><br>
+		Enter Emp Name :  <input type="text" name="empname"> <br><br>
+		Enter City :	  <input type="text" name="city"> <br><br>
+		Enter Salary :	  <input type="text" name="salary"> <br><br>
+
+					 	  <input type="submit" name="submit" value="submit">
+	</form>
+
+</body>
+</html>
+
